6eaa7ed425421707712a694d9d8b17e7027e2cc6
Chris Cankov <cankov.chris@gmail.com>
2017-08-16 16:57:47 -0700

    Change port back

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,7 +69,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
-    port:                 465,
+    port:                 587,
     domain:               'fotoplr.herokuapp.com',
     user_name:            ENV['GMAIL_USER'],
     password:             ENV['GMAIL_PASSWORD'],
