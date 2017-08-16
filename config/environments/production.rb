557dda71303f28255ec0932848ea0bba17ad78ff
Chris Cankov <cankov.chris@gmail.com>
2017-08-16 16:54:09 -0700

    Try different SMTP port in prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,7 +69,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
-    port:                 587,
+    port:                 465,
     domain:               'fotoplr.herokuapp.com',
     user_name:            ENV['GMAIL_USER'],
     password:             ENV['GMAIL_PASSWORD'],
